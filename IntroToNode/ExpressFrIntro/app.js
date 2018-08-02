6fba4a82cc354501feb898a4017e561489198e01
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-02 15:04:08 +0300

    added express routing learning

--- IntroToNode/ExpressFrIntro/app.js
+++ IntroToNode/ExpressFrIntro/app.js
@@ -18,6 +18,43 @@ app.get("/dog", function(req, res) {
   res.send("Woof");
 });
 
+// route params pattern
+app.get("/r/:subredditName/:title", function(req, res) {
+  console.log(req.params);
+  var subreddit = req.params.subredditName;
+  res.send(`welcome to ${subreddit}`);
+});
+
+//exercise1
+app.get("/speak/:animal", function(req, res) {
+  var animal = req.params.animal.toLowerCase();
+  var sounds = {
+    pig: "Oink",
+    cow: "Moo",
+    dog: "Woof"
+  }
+  var sound = sounds[animal];
+  res.send(`The ${animal} says ${sound}`);
+});
+
+// exercise2
+app.get("/repeat/:message/:times", function(req, res) {
+  var message = req.params.message;
+  var times = Number(req.params.times);
+  var result = "";
+
+  for(var i=0; i<times; i++) {
+    result += message + " ";
+  }
+
+  res.send(result);
+});
+
+// all routes - order important: must be last
+app.get("*", function(req, res) {
+  res.send("all not defined routes");
+});
+
 // Tell express to listen for requests (start server)
 app.listen(3000, () => 
-console.log('Example app listening on port 3000!'))
+console.log('Example app listening on port 3000!'));
