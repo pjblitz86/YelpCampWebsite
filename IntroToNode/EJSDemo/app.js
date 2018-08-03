a6fd1295fe23330d3c888c3d222b3acc0b642ed4
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-03 14:40:47 +0300

    ejs example with if and for loops

--- IntroToNode/EJSDemo/app.js
+++ IntroToNode/EJSDemo/app.js
@@ -6,5 +6,14 @@ app.get("/yo/:thing", function(req, res) {
   res.render("home.ejs", {thingVar:thing});
 });
 
+app.get("/posts", function(req, res) {
+  var posts = [
+    {title: "Post1", author: "PJ"},
+    {title: "Post2", author: "AJ"},
+    {title: "Post3", author: "MJ"}
+  ];
+  res.render("posts.ejs", {posts:posts});
+});
+
 app.listen(3000, () => 
 console.log('Example app listening on port 3000!'));
