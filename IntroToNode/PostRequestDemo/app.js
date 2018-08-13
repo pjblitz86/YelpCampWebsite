34f3247baa50c2339e77b4ecc811b8de563e2ff9
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-13 13:52:53 +0300

    finished PostRequestDemo

--- IntroToNode/PostRequestDemo/app.js
+++ IntroToNode/PostRequestDemo/app.js
@@ -1,20 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+var friends = ["PJ", "AJ", "NJ"];
 
 app.get("/", function(req, res) {
   res.render("home");
 });
 
 app.get("/friends", function(req, res) {
-  var friends = ["PJ", "AJ", "NJ"];
+  
   res.render("friends", {friends: friends});
 })
 
 // POST REQUEST
 app.post("/addfriend", function(req, res) {
-  res.send("post route");
+  var newFriend = req.body.newfriend;
+  friends.push(newFriend);
+  res.redirect("friends");
 });
 
 app.listen(3000, () => 
