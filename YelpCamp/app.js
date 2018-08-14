500ae6b325c9301a2e765c1bf5b2d882131b8444
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-14 10:17:52 +0300

    styling campgrounds page by adding bootstrap jumbotron and grid

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -9,6 +9,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 var campgrounds = [
   {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg"},
   {name: "Granite Hill", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+  {name: "Goat Mountain Rest", image: "https://farm9.staticflickr.com/8572/16034357695_5ca6214f59.jpg"},
+  {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg"},
+  {name: "Granite Hill", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
   {name: "Goat Mountain Rest", image: "https://farm9.staticflickr.com/8572/16034357695_5ca6214f59.jpg"}
 ];
 
