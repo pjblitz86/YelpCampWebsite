1b73c8366d0cefed7fdf81b58425c95d9e7ca420
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-17 10:58:17 +0300

    implemented comment POST route

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,7 +3,8 @@ var express     = require('express'),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose'),
     Campground  = require("./models/campground"),
-    seedDB      = require("./seeds");
+    seedDB      = require("./seeds"),
+    Comment     = require("./models/comment");
 
 mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
@@ -79,9 +80,32 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
     } else {
       res.render("comments/new", {campground: campground});
     }
+  });  
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+  // lookup campground using id
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      // create new comment
+      Comment.create(req.body.comment, function(err, comment) {
+        if(err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/"+campground._id);
+        }
+      });
+    }
   });
   
-})
+  // connect new comment to campground
+  // redirect to campground show page
+});
 
 
 app.listen(3000, () => 
