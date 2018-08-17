cc9eb50d6a64a159ec870303b4680b9fc8154501
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-17 09:56:51 +0300

    implemented db seeding

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,27 +2,15 @@ var express     = require('express'),
     app         = express(),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose'),
-    Campground  = require("./models/campground");
+    Campground  = require("./models/campground"),
+    seedDB      = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true });
-
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-// Campground.create(
-//   { 
-//     name: "Goat Mountain Rest", 
-//     image: "https://farm9.staticflickr.com/8572/16034357695_5ca6214f59.jpg",
-//     description: "There are many screaming goats here... MEEEEEEE... AAAAAAAHHH!"
-//   }, function(err, campground) {
-//     if(err) {
-//       console.log(err);
-//     } else {
-//       console.log("NEWLY CREATED CAMPGROUND: ");
-//       console.log(campground);
-//     }
-//   });
+// seed the db with starter data
+seedDB();
 
 // RESTFUL ROUTES
 
@@ -68,7 +56,7 @@ app.post("/campgrounds", function(req, res) {
 // GET: campground show page by id
 app.get("/campgrounds/:id", function(req, res) {
   // find campground with provided ID
-  Campground.findById(req.params.id, function(err, foundCampground) {
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
     if(err) {
       console.log(err);
     } else {
