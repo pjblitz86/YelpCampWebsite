8dff89bcaf457250197c737f76207e4095884254
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-17 16:15:59 +0300

    implemented login template with get and post routes

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -126,6 +126,8 @@ app.post("/campgrounds/:id/comments", function(req, res) {
 //==============
 // AUTH ROUTES
 //==============
+
+// REGISTER get and post
 app.get('/register', function(req, res) {
   res.render("register");
 });
@@ -141,6 +143,19 @@ app.post('/register', function(req, res) {
       res.redirect("/campgrounds");
     });
   });
+});
+
+// LOGIN get and post
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+// post login with middleware
+app.post("/login", passport.authenticate("local", 
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(req, res) {
 });
 
 app.listen(3000, () => 
