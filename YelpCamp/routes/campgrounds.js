746af0a39134599ed238d778c4f47acd42d9a74a
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-20 10:43:18 +0300

    refactoring and associating newly created campground with logged in user

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -15,12 +15,16 @@ router.get("/", function(req, res) {
 });
 
 // POST: form submit - create new campground
-router.post("/", function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
   // get data from form and add to db
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc};
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
+  var newCampground = {name: name, image: image, description: desc, author: author};
   // Create a new camground and save to db
   Campground.create(newCampground, function(err, newlyCreared) {
     if(err) {
@@ -33,7 +37,7 @@ router.post("/", function(req, res) {
 });
 
 // GET: form show route
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
   res.render("campgrounds/new");
 });
 
@@ -49,5 +53,13 @@ router.get("/:id", function(req, res) {
     }
   });
 });
+
+// MIDDLEWARE
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
