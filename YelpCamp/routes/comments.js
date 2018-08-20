991a728ab61eecbb7b0548f09af134c5c8afd19f
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-20 16:48:04 +0300

    Comment Authorization added

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -45,7 +45,7 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 // EDIT
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, foundComment) {
     if(err) {
       res.redirect("back");
@@ -56,7 +56,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // UPDATE
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
     if(err) {
       res.redirect('back');
@@ -67,7 +67,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // DESTROY
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
   // find by id and remove
   Comment.findByIdAndRemove(req.params.comment_id, function(err) {
     if(err) {
@@ -84,6 +84,26 @@ function isLoggedIn(req, res, next) {
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+  // is user logged in?
+  if(req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if(err) {
+        res.redirect("back");
+      } else {
+      // does user own a campground
+        if(foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
