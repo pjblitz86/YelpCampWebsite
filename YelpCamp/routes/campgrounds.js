5dd6896721b4ffdabd566765405f729e0b29d08f
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-20 13:10:35 +0300

    CRUD - implemented destroy

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -78,6 +78,17 @@ router.put("/:id", function(req, res) {
   // redirect to show page
 });
 
+// DESTROY
+router.delete("/:id", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if(err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 // MIDDLEWARE
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
