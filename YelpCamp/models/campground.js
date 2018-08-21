1ad4330039e486f455d5dc1bd8d56ab4e4946f62
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-21 13:26:57 +0300

    added dynamic camp price feature

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 // Set the db schema and model
 var campgroundSchema = new mongoose.Schema({
   name: String,
+  price: String,
   image: String,
   description: String,
   author: {
--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -20,12 +20,13 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
   // get data from form and add to db
   var name = req.body.name;
   var image = req.body.image;
+  var price = req.body.price;
   var desc = req.body.description;
   var author = {
     id: req.user._id,
     username: req.user.username
   }
-  var newCampground = {name: name, image: image, description: desc, author: author};
+  var newCampground = {name: name, price: price, image: image, description: desc, author: author};
   // Create a new camground and save to db
   Campground.create(newCampground, function(err, newlyCreated) {
     if(err) {
